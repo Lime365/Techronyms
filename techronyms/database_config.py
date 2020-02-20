9742db363aca3007fccd834fade3345a2ac2d46d
Lime365 <60894523+Lime365@users.noreply.github.com>
2020-02-20 18:52:24 +0100

    Update database_config.py

--- techronyms/database_config.py
+++ techronyms/database_config.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 from sqlite3 import Error
 
-project_path = r"C:/Users/Lime/pyProjects/tech_abs_quiz"
+project_path = r"C:/Users/Lime/pyProjects/techronyms"
 
 #Create our SQL connection
 def sq_connect(db_file):
@@ -23,9 +23,9 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print (e)
 
-def main_create():
-    database = f"{project_path}/abbs.db"
-
+def main_create_table():
+    database = f"{project_path}/acronyms.db"
+#----------- Modify to create new table if needed ------------#
     sql_make_table = """CREATE TABLE IF NOT EXISTS scoreboard
                         (id integer PRIMARY KEY,
                         nick NOT NULL,
@@ -40,23 +40,25 @@ def main_create():
     else:
         print('Error!')
 
-def add_question(conn, q):
+def make_query(conn, q):
+    #---------------- Modify to make a new entry to the database ---------------#
     sql = '''INSERT INTO scoreboard(nick, score, date) VALUES(?,?,?)'''
     c = conn.cursor()
     c.execute(sql, q)
     return c.lastrowid
 
-def main_create_base():
-    database = f'{project_path}/abbs.db'
+def main_add_entry():
+    database = f'{project_path}/acronyms.db'
 
     conn = sq_connect(database)
     with conn:
-        for i in range(10,21):
-            q = ('test'+str(i), i, '2020-02-17');
-            query_id = add_question(conn, q)
+        q = ('nick', 'score', '2020-02-17');
+        query_id = make_query(conn, q)
 
 
 
 if __name__ == '__main__':
+# Run main_create_table() to create table or main_add_entry() to add a new entry
 
-    main_create_base()
+    #main_create_table()
+    #main_add_entry()
